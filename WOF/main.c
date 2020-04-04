28b06ab2539ad0e1fa9aa6877a984cd30b18461c
CHEN Xiao Zhou Gilles <019161962A@uni.lu>
2020-04-04 15:39:17 +0200

    Initialized project and created a main file thats prints something.

---
+++ WOF/main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: 019161962A
+ *
+ * Created on 4 April 2020, 15:37
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main() {
+
+    printf("Hello Project - Gilles Chen");
+}
+
