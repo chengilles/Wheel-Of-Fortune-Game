4758c5747add0d0f4fd27e4557b092f5b472e4bf
CHEN Xiao Zhou Gilles <019161962A@uni.lu>
2020-04-04 12:00:39 +0200

    Initial Commit

---
+++ main.c
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: 019161962A
+ *
+ * Created on 4 April 2020, 11:44
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main() {
+
+    printf("Hello World - Gilles Chen");
+    
+}
+
