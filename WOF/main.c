72c9a1edd21e118df1f0ce381aec2cd788bc756f
CHEN Xiao Zhou Gilles <019161962A@uni.lu>
2020-04-11 16:08:37 +0200

    Removed Netbeans autogenerated Header on file.

--- WOF/main.c
+++ WOF/main.c
@@ -1,16 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.c
- * Author: 019161962A
- *
- * Created on 4 April 2020, 15:37
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
