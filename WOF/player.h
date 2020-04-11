2bb44e600747a85c2d5c7257043ec528e0ae7b7a
CHEN Xiao Zhou Gilles <019161962A@uni.lu>
2020-04-11 16:05:59 +0200

    Create player.h Header file and define method bodies

---
+++ WOF/player.h
@@ -0,0 +1,23 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+typedef struct{
+    char* name;
+    int currentTurnMoney = 0;
+    int totalMoney = 0;
+} Player;
+
+//Returns the number of players.
+int getPlayersNumber();
+
+//Returns the names of the players.
+char* getPlayerNames;
+
+//Returns a random integer to decide which player will start the first Turn.
+int chooseFirstPlayer();
+
+//Returns an Array of Pointers to a structure Player.
+Player** initPlayers();
+
+#endif /* PLAYER_H */
+
