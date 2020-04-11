d0dd8b3c2e13e20cd346fe52230ceec528b63a5e
COSSA Hugo <hugo.cossa.001@student.uni.lu>
2020-04-11 16:22:49 +0200

    Create Wheel.h Header file and define methods

---
+++ WOF/Wheel.h
@@ -0,0 +1,21 @@
+#ifndef WHEEL_H
+#define WHEEL_H
+
+// Compares input with currrent puzzle
+bool solvePuzzle();
+
+// Checks if a letter is present and returns number of occurences
+int isPresent();
+
+// Returns number of occurences and reveal letters
+int revealLetter();
+
+// Checks if the vowel is in the puzzle 
+bool buyVowel();
+
+// Returns a random number and takes the correspondance value in the array 
+int spinWheel();
+
+#endif /* WHEEL_H */
+
+
