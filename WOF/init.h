5d6857900e2f842c65f619d6a4e1fadab227edb9
CHEN Xiao Zhou Gilles <019161962A@uni.lu>
2020-04-11 16:06:15 +0200

    Create init.h Header file and define method bodies

---
+++ WOF/init.h
@@ -0,0 +1,14 @@
+#ifndef INIT_H
+#define INIT_H
+
+//Displays the rules of the WOF game.
+void displayRules();
+
+//Takes 3 sentences input from the User/Host to use in the WOF game.
+char** getPuzzles();
+
+//Displays Greetings at the start of the WOF game.
+void displayGreetings();
+
+#endif /* INIT_H */
+
