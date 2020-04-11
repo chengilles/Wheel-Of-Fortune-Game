ceaed47813f48430c604a8a54d2e1146bcd1517f
COSSA Hugo <hugo.cossa.001@student.uni.lu>
2020-04-11 16:29:20 +0200

    Fixing file name

--- WOF/Wheel.h
+++ WOF/Wheel.h
@@ -1,19 +1,19 @@
 #ifndef WHEEL_H
 #define WHEEL_H
 
-// Compares input with currrent puzzle
+// Compares input with currrent puzzle.
 bool solvePuzzle();
 
-// Checks if a letter is present and returns number of occurences
+// Checks if a letter is present and returns number of occurences.
 int isPresent();
 
-// Returns number of occurences and reveal letters
+// Returns number of occurences and reveal letters.
 int revealLetter();
 
-// Checks if the vowel is in the puzzle 
+// Checks if the vowel is in the puzzle.
 bool buyVowel();
 
-// Returns a random number and takes the correspondance value in the array 
+// Returns a random number and takes the correspondance value in the array.
 int spinWheel();
 
 #endif /* WHEEL_H */
