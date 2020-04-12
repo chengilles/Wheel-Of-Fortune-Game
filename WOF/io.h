bd0b1a53c6f26d706b672ae1c814e533ec2d8eeb
COSSA Hugo <hugo.cossa.001@student.uni.lu>
2020-04-12 19:04:18 +0200

    Fixing issues with io.h

--- WOF/io.h
+++ WOF/io.h
@@ -1,11 +1,18 @@
 #ifndef IO_H
 #define IO_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER_SIZE 20
+
 //Gets the input and returns it.
-char* my_gets();
+char* my_gets(int maxLength);
 
 //Flushes input buffer.
-void fflush();
-        
+void flushInput(void);
+
 #endif /* IO_H */
+
 
