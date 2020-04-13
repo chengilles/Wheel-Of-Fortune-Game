34d353c0a0b5d6bc95ac881d7f738702d404acd5
CHEN Xiao Zhou Gilles <019161962A@uni.lu>
2020-04-13 14:08:36 +0200

    Implemented getOccurrence

--- WOF/wheel.c
+++ WOF/wheel.c
@@ -1,4 +1,5 @@
 #include "wheel.h"
+#include <string.h>
 
 bool isVowel(char c){
     if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
@@ -6,3 +7,19 @@ bool isVowel(char c){
     }
     return false;
 }
+
+int getOccurrence(char *puzzle,char *currentPuzzle, int c){
+    int occurrence = 0;
+    for(int i = 0; i < strlen(puzzle); i++){
+        if(c == puzzle[i]){
+            occurrence++;
+            currentPuzzle[i] = c;
+        }
+    }
+    return occurrence;
+}
+
+    
+   
+    
+    
