fbb627960e5107ed02e98431d696cfca6fde0e1d
Thomas <gillet.thomas43@gmail.com>
2020-04-25 15:42:31 +0200

    Add includes in main.c

--- WOF/main.c
+++ WOF/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "init.h"
+#include "io.h"
+//#include "player.h"
+#include "turn.h"
+#include "wheel.h"
 
-/*
- * 
- */
 int main() {
 
     printf("Hello Project - Gilles Chen\n");
