afd2415972ec26e68074efdab74fc93270715c6b
Thomas <gillet.thomas43@gmail.com>
2020-04-25 16:27:12 +0200

    Tweak getSentences in init.c

--- WOF/init.c
+++ WOF/init.c
@@ -1,7 +1,7 @@
 #include "init.h"
 
 void displayRules() {
-	printf("At each turn you have these options : spin the wheel and call a consonant. If your letter was in the puzzle you can then buy a vowel for $250, spin the wheel again or try to solve the puzzle. Each consonant is worth the money value of the field you hit. Contestants can continue spinning the wheel until they guess a wrong letter or hit Bankrupt / Lose a Turn.");
+	printf("\n\nAt each turn you have these options : spin the wheel and call a consonant. If your letter was in the puzzle you can then buy a vowel for $250, spin the wheel again or try to solve the puzzle. Each consonant is worth the money value of the field you hit. Contestants can continue spinning the wheel until they guess a wrong letter or hit Bankrupt / Lose a Turn.\n");
 }
 
 void displayGreetings() {
@@ -9,13 +9,11 @@ void displayGreetings() {
 }
 
 char** getPuzzles() {
-    char** sentences = NULL;
-    char* input;
+    char* sentences[3];
 
     for (int i = 0; i < 3; i++){
-        printf("Enter the sentence %d :", i+1);
-        input = _strdup(my_gets(BUFFER_SIZE));
-        sentences[i] = input;
+        printf("\nEnter the sentence %d: ", i+1);
+        sentences[i] = _strdup(my_gets(BUFFER_SIZE));
     }
 
     return sentences;
