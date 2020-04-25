347d4453e32fde854f777f71ff578cd1222f7b93
Thomas <gillet.thomas43@gmail.com>
2020-04-25 15:51:54 +0200

    Add header inclusion in main.c

--- WOF/main.c
+++ WOF/main.c
@@ -8,9 +8,7 @@
 
 int main() {
 
-    printf("Hello Project - Gilles Chen\n");
-    printf("Hello Project - Thomas Gillet\n");
-    printf("Hello Project - Hugo Cossa\n");
+    displayGreetings();
 
     return 0;
 }
