eb45ca33984146bccbb41465dadd006d46feb3b1
CHEN Xiao Zhou Gilles <xiao.chen.001@student.uni.lu>
2020-05-13 22:28:54 +0200

    Tweak to getPlayersNumber - doesn't wait for enter anymore

--- WOF/player.c
+++ WOF/player.c
@@ -5,7 +5,7 @@ int getPlayersNumber() {
 
 	do {
 		printf("\nHow many players will be playing ? (2 or 3) ");
-		scanf_s("%d", &numberOfPlayers);
+		if(scanf_s("%d", &numberOfPlayers) == 0)
 		while (getchar() != '\n');
 	} while (numberOfPlayers != 2 && numberOfPlayers != 3);
 
